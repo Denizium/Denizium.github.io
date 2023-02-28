9c949b4bfdd0be84a065c36d3dfd89c953f7765e
denizium <d.mam4ur@gmail.com>
2023-02-28 18:49:42 +0200

    modified hw25

--- hw25/js/index.js
+++ hw25/js/index.js
@@ -1,3 +1,3 @@
 const usertime = prompt("Write any number of hours to convert to seconds")
-const user = (usertime * 60 * 60 + " seconds")
+let user = (usertime * 60 * 60 + " seconds")
 alert (user)
