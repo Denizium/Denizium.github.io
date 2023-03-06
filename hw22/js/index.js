72d7af3e09e842c67fdc4d78abcb8b3d2db15f93
denizium <d.mam4ur@gmail.com>
2023-03-06 20:56:21 +0200

    modified hw22/js/index.js

--- hw22/js/index.js
+++ hw22/js/index.js
@@ -1,9 +1,9 @@
-const username = prompt("What is your name")
-const usersurname = prompt("What is your surname")
-const userage = prompt("How old are you")
-const user =`${username} ${usersurname} ${userage}`
+const userName = prompt("What is your name")
+const userSurname = prompt("What is your surname")
+const userAge = prompt("How old are you")
+const user =`${userName} ${userSurname} ${userAge}`
 alert(user)
 // const namber = ("5"+" 4"+" 3"+" 2"+" 1");
 // console.log(namber);
 const namber = "54321";
-console.log(namber.split(""));
+console.log(namber.split("").join(" "));
