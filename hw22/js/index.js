c45363e8fddd560cfd3b05733bcaa0a645c716e6
denizium <d.mam4ur@gmail.com>
2023-03-06 20:14:07 +0200

    add hw22

---
+++ hw22/js/index.js
@@ -0,0 +1,9 @@
+const username = prompt("What is your name")
+const usersurname = prompt("What is your surname")
+const userage = prompt("How old are you")
+const user =`${username} ${usersurname} ${userage}`
+alert(user)
+// const namber = ("5"+" 4"+" 3"+" 2"+" 1");
+// console.log(namber);
+const namber = "54321";
+console.log(namber.split(""));
