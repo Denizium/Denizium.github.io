25d94528b0d744238d11a1aa3b061bac3cbf7c56
denizium <d.mam4ur@gmail.com>
2023-03-07 21:10:47 +0200

    add hw23

---
+++ hw23/js/index.js
@@ -0,0 +1,5 @@
+const userNumber1 = prompt("Please, enter any number to add")
+const userNumber2 = prompt("Please, enter the second number to add")
+let add = Number(userNumber1) + Number(userNumber2);
+let user = `${userNumber1} + ${userNumber2} = ${add}`;
+alert (user)
