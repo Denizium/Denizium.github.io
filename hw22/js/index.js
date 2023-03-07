31e1c94fe435081f6c06e5a2edaeb94fb44d3735
denizium <d.mam4ur@gmail.com>
2023-03-07 17:53:59 +0200

    fixed bugs in hw22

--- hw22/js/index.js
+++ hw22/js/index.js
@@ -3,7 +3,5 @@ const userSurname = prompt("What is your surname")
 const userAge = prompt("How old are you")
 const user =`${userName} ${userSurname} ${userAge}`
 alert(user)
-// const namber = ("5"+" 4"+" 3"+" 2"+" 1");
-// console.log(namber);
-const namber = "54321";
-console.log(namber.split("").join(" "));
+const number = "54321";
+console.log(number.split("").join(" "));
