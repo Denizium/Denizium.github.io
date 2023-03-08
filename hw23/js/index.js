9e1b062b645dc3e1f51970c1298e38d5b2aa6294
denizium <d.mam4ur@gmail.com>
2023-03-08 16:12:45 +0200

    modified index.js

--- hw23/js/index.js
+++ hw23/js/index.js
@@ -1,5 +1,8 @@
-const userNumber1 = prompt("Please, enter any number to add")
-const userNumber2 = prompt("Please, enter the second number to add")
+const userNumber1 = prompt("Please, enter the first random number")
+const userNumber2 = prompt("Please, enter a second random number")
 let add = Number(userNumber1) + Number(userNumber2);
-let user = `${userNumber1} + ${userNumber2} = ${add}`;
-alert (user)
+let subtraction = Number(userNumber1) - Number(userNumber2);
+let multiplication = Number(userNumber1) * Number(userNumber2);
+let division = Number(userNumber1) / Number(userNumber2);
+let user = `${userNumber1} + ${userNumber2} = ${add} \n ${userNumber1} - ${userNumber2} = ${subtraction} \n ${userNumber1} * ${userNumber2} = ${multiplication} \n ${userNumber1} / ${userNumber2} = ${division} \n`;
+alert(user)
