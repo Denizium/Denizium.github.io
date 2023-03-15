7bebf5acb145882ced835a36b794346f43c1ee23
denizium <d.mam4ur@gmail.com>
2023-03-15 16:25:20 +0200

    fixed bugs in hw27

--- hw27/js/index.js
+++ hw27/js/index.js
@@ -7,10 +7,10 @@ if (userCity === "Kyiv" || userCity === "kyiv") { userCity = 'You live in the ca
 else if (userCity === "Washington" || userCity === "washington") { userCity = 'You live in the capital USA' }
 else if (userCity === "London" || userCity === "london") { userCity = 'You live in the capital England' }
 else if (userCity === null) { userCity = 'It is a pity that you did not want to enter your city' }
-else { userCity = `You live in ${userCity}` };
+else { userCity = `You live in ${userCity}` }
 if (userSport === "Box" || userSport === "box") { userSport = 'Cool, you want to become Vitali Klitschko' }
 else if (userSport === "Chess" || userSport === "chess") { userSport = 'Cool you want to become Magnus Carlsen' }
 else if (userSport === "Table tennis" || userSport === "table tennis") { userSport = 'Cool, you want to become Fang Jaedong' }
 else if (userSport === null) { userSport = 'It is a pity that you did not want to enter your favourite sport' }
-else { userSport = `You favourite sport ${userSport}` };
+else { userSport = `You favourite sport ${userSport}` }
 alert(`${age};\n ${userCity};\n ${userSport}.`);
