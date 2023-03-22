a5dcdb18f980440122ad722046f6da125e4c7bf2
denizium <d.mam4ur@gmail.com>
2023-03-22 15:23:38 +0200

    add hw29

---
+++ hw29/js/index.js
@@ -0,0 +1,77 @@
+for (let number = 10; number <= 20; number++) {
+    console.log(number + ",");
+}
+for (let mult = 10; mult <= 20; mult++) {
+    console.log(mult * mult);
+}
+for (let multSev = 0; multSev <= 10; multSev++) {
+    console.log(`7 * ${multSev} = ${multSev * 7}`);
+}
+let summ = 0;
+for (let i = 0; i <= 15; i++) {
+    summ += i;
+}
+console.log(summ);
+let mult = 1;
+for (let i = 15; i <= 35; i++) {
+    mult *= i;
+}
+console.log(mult);
+let summa = 0;
+for (let i = 1; i <= 500; i++) {
+    summa += i;
+}
+console.log(Math.floor(summa / 500));
+let sum = 0;
+for (let i = 30; i <= 80; i += 2) {
+    sum += i;
+}
+console.log(sum);
+for (let number = 103; number <= 199; number += 3) {
+    console.log(number);
+}
+let natur = 9;
+for (let number = 1; number <= 9; number++) {
+    console.log(natur / number);
+}
+let naturPar = 9;
+for (let i = 2; i <= 8; i += 2) {
+    console.log(naturPar / i);
+}
+// //================================================================
+let naturParSum = 9;
+for (let i = 2; i <= 8; i += 2) {
+    naturParSum += naturParSum / i;
+}
+console.log(naturParSum);
+//================================================================
+for (let multone = 1; multone <= 10; multone++) {
+    console.log(`1 * ${multone} = ${multone}`);
+}
+for (let multtwo = 1; multtwo <= 10; multtwo++) {
+    console.log(`2 * ${multtwo} = ${multtwo * 2}`);
+}
+for (let multThree = 1; multThree <= 10; multThree++) {
+    console.log(`3 * ${multThree} = ${multThree * 3}`)
+}
+for (let multFour = 1; multFour <= 10; multFour++) {
+    console.log(`4 * ${multFour} = ${multFour * 4}`)
+}
+for (let multFive = 1; multFive <= 10; multFive++) {
+    console.log(`5 * ${multFive} = ${multFive * 5}`)
+}
+for (let multSix = 1; multSix <= 10; multSix++) {
+    console.log(`6 * ${multSix} = ${multSix * 6}`)
+}
+for (let multSeven = 1; multSeven <= 10; multSeven++) {
+    console.log(`7 * ${multSeven} = ${multSeven * 7}`)
+}
+for (let multEight = 1; multEight <= 10; multEight++) {
+    console.log(`8 * ${multEight} = ${multEight * 8}`)
+}
+for (let multNine = 1; multNine <= 10; multNine++) {
+    console.log(`9 * ${multNine} = ${multNine * 9}`)
+}
+for (let multTen = 1; multTen <= 10; multTen++) {
+    console.log(`10 * ${multTen} = ${multTen * 10}`)
+}
